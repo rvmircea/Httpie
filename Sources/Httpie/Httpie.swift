b0e94704a94c9644c1cb732762d94d325b0b36a3
radumircea <vmircea1771@gmail.com>
2025-06-17 19:08:25 +0300

    Initial commit

--- Sources/Httpie/Httpie.swift
+++ Sources/Httpie/Httpie.swift
@@ -1,2 +1,30 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public actor Httpie {
+    private let baseAddress: String
+    private let decoder: JSONDecoder
+    private let encoder: JSONEncoder
+    
+    init(baseAddress: String) {
+        self.baseAddress = baseAddress
+        self.decoder = JSONDecoder()
+        self.encoder = JSONEncoder()
+    }
+    
+    public func getFromJson<T: Codable> (endpoint: String) async throws -> T? {
+        do {
+            guard let url = URL(string: "\(baseAddress)\(endpoint)") else { return nil }
+            var request = URLRequest(url: url)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let (data, _) = try await URLSession.shared.data(for: request)
+            
+            return try self.decoder.decode(T.self, from: data)
+        } catch {
+            return nil
+        }
+    }
+}
--- Tests/HttpieTests/HttpieTests.swift
+++ Tests/HttpieTests/HttpieTests.swift
@@ -2,5 +2,20 @@ import Testing
 @testable import Httpie
 
 @Test func example() async throws {
+    struct TestingStruct: Codable {
+        var message: String = ""
+    }
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let httpClient = Httpie(baseAddress: "http://localhost:5117")
+    
+    await repeating(times: 3) {
+        let result: TestingStruct? = try! await httpClient.getFromJson(endpoint: "/car")
+        #expect(result?.message == "Hello, car!")
+    }
+}
+
+func repeating(times n: UInt8, _ action: () async -> Void) async {
+    for _ in 0..<n {
+        await action()
+    }
 }
